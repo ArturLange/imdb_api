ab3c28680ce898172e394da6200034eaec9da14f
Artur H. Lange <ArturLange@poczta.fm>
2018-10-15 18:50:08 +0200

    Add postgresql support

--- flask_app.py
+++ flask_app.py
@@ -2,26 +2,10 @@ from flask import Flask, jsonify
 from models import Base, Title, Name
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from database import init_db
 
 app = Flask(__name__)
 
-# bind SQLAlchemy
-engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
-db_session = scoped_session(
-    sessionmaker(
-        autocommit=False,
-        autoflush=False,
-        bind=engine
-    )
-)
-
-
-def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_db()
-    import models
-    Base.metadata.create_all(bind=engine)
 
 
 @app.route('/titles', methods=['GET'])
