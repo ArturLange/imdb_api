6866a0f34efc30ce9aa1756e272e4a196a11dbed
Artur H. Lange <ArturLange@poczta.fm>
2018-10-15 20:40:34 +0200

    Add names endpoint

--- flask_app.py
+++ flask_app.py
@@ -26,22 +26,36 @@ def get_titles_from_year(startYear):
     )
 
 
-@app.route('/titles', methods=['GET'])
-def get_titles():
-    titles = Title.query.all()
+@app.route('/names', methods=["GET"])
+def get_people_and_their_titles():
+    names = Name.query
+
+    primaryName = request.args.get('primaryName')
+    birthYear = request.args.get('birthYear')
+    deathYear = request.args.get('deathYear')
+    primaryProfession = request.args.get('primaryProfession')
+
+    if primaryName:
+        names = names.filter(Name.primaryName.ilike(f'%{primaryName}%'))
+    if birthYear:
+        names = names.filter(Name.birthYear == birthYear)
+    if deathYear:
+        names = names.filter(Name.deathYear == deathYear)
+    if primaryProfession:
+        names = names.filter(
+            Name.primaryProfession.ilike(f'%{primaryProfession}%'))
+
+    maxResults = request.args.get('maxResults')
+    page = request.args.get('page')
+
+    if maxResults:
+        names = names.limit(maxResults)
+        if page:
+            names = names.offset((int(page)-1) * int(maxResults))
+
     return jsonify(
         [
-            title.to_dict() for title in titles
-        ]
-    )
-
-
-@app.route('/names', methods=['GET'])
-def get_names():
-    names = Name.query.all()
-    return jsonify(
-        [
-            name.to_dict() for name in names
+            name.to_dict() for name in names.order_by(Name.primaryName)
         ]
     )
 
